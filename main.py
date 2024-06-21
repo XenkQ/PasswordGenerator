9f4997f1116b33c40c9d8e2394a7e9b787cae6ab
Konrad Blaszyński <77151114+XenkQ@users.noreply.github.com>
2024-06-21 15:58:46 +0200

    fixing generated gui - refactoring and adding adding button functionality

--- main.py
+++ main.py
@@ -2,10 +2,8 @@ import random
 import string
 import application
 from password import Password
+import gui.gui
 
 if __name__ == "__main__":
-    app = application.Application()
-    app.master.title(application.APP_TITLE)
-    app.master.minsize(application.WINDOW_MIN_WIDTH, application.WINDOW_MIN_HEIGHT)
-    app.master.grid_size()
-    app.mainloop()
+    print(Password.generate_password(3,3,3, 'xyz'))
+    pass
--- password.py
+++ password.py
@@ -1,21 +1,26 @@
 import string
 import random
 
+CHARACTERS_BAD_FOR_PASSWORD = "0O1 Il5S<>[]{}|\\/^~`\"'"
 
 class Password:
-
     @staticmethod
-    def generate_password(length=8, use_letters=True, use_digits=True, use_punctuation=True) -> str:
+    def generate_password(letters_count:int, digits_count:int, punctuation_count:int, exclude_characters="") -> str:
         password, characters = "", ""
+        password_length = letters_count + digits_count + punctuation_count
 
-        if use_letters:
+        if letters_count:
             characters += string.ascii_letters
-        if use_digits:
+        if digits_count:
             characters += string.digits
-        if use_punctuation:
+        if punctuation_count:
             characters += string.punctuation
 
-        for _ in range(length):
-            password = ''.join(random.choice(characters) for _ in range(length))
+        for c in exclude_characters:
+            if c in characters:
+                characters.replace(c, '')
+
+        for _ in range(password_length):
+            password = ''.join(random.choice(characters) for _ in range(password_length))
 
         return password
