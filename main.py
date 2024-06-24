583b621de952ded16538ec551c04ddfc3668b5aa
Konrad Blaszyński <77151114+XenkQ@users.noreply.github.com>
2024-06-24 14:24:08 +0200

    changing pwd generation and connecting gui buttons to appropriate functions

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import random
-import string
-import application
-from password import Password
-import gui.gui
+from gui import GUI
 
 if __name__ == "__main__":
-    print(Password.generate_password(3,3,3, 'xyz'))
-    pass
+    app = GUI()
--- password.py
+++ password.py
@@ -1,26 +1,41 @@
 import string
 import random
 
+#similar characters to exclude on default
 CHARACTERS_BAD_FOR_PASSWORD = "0O1 Il5S<>[]{}|\\/^~`\"'"
 
+
 class Password:
+    __character_sets = None
+    __previously_excluded = CHARACTERS_BAD_FOR_PASSWORD
+
     @staticmethod
-    def generate_password(letters_count:int, digits_count:int, punctuation_count:int, exclude_characters="") -> str:
-        password, characters = "", ""
-        password_length = letters_count + digits_count + punctuation_count
+    def generate_password(lower_letters_count: int, upper_letters_count: int, digits_count: int, special_signs_count: int, exclude_characters="") -> str:
+        sets_length = [lower_letters_count, upper_letters_count, digits_count, special_signs_count]
+        password = ""
 
-        if letters_count:
-            characters += string.ascii_letters
-        if digits_count:
-            characters += string.digits
-        if punctuation_count:
-            characters += string.punctuation
+        if Password.__character_sets is None or Password.__previously_excluded != exclude_characters:
+            Password.__character_sets = {
+                "lower_letters": string.ascii_lowercase,
+                "upper_letters": string.ascii_uppercase,
+                "digits": string.digits,
+                "special_characters": string.punctuation
+            }
 
-        for c in exclude_characters:
-            if c in characters:
-                characters.replace(c, '')
+            for key in Password.__character_sets:
+                new_set = []
 
-        for _ in range(password_length):
-            password = ''.join(random.choice(characters) for _ in range(password_length))
+                characters = Password.__character_sets[key]
+                for c in characters:
+                    if c not in exclude_characters:
+                        new_set.append(c)
+
+                Password.__character_sets[key] = "".join(new_set)
+
+            Password.__previously_excluded = exclude_characters
+
+        for i, key in enumerate(Password.__character_sets):
+            if sets_length[i]:
+                password += ''.join(random.choice(Password.__character_sets[key]) for _ in range(sets_length[i]))
 
         return password
